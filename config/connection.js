de4f2715203fa5176a3e16d10317105ced33e79e
Yali Miranda <yalimiranda@gmail.com>
2020-01-21 14:33:38 -0800

    added connection to JAWS_DB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "pizzas_db"
-});
+var connection;
+
+if(process.env.JAWS_URL){
+  connection = mysql.createConnection(process.env.JAWS_URL);
+} else{
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "pizzas_db"
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
